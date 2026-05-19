b951fe1c87461bfd571acc69a2cfaceca47e56cb
hagai.tregerman <hagai1973@gmail.com>
2026-05-19 19:21:42 +0300

    test

---
+++ Strings/basic_strings_commands.py
@@ -0,0 +1,15 @@
+str1 = "Hello world"
+
+print(str1[0])  # H
+print(str1[1])  # e
+print(str1[2])  # l
+print(str1[3])  # l
+print(str1[4])  # o
+print(str1[-1])  # d
+
+s_name = "John Doe"
+print(len(s_name))  # 8
+
+print(s_name.upper())  # JOHN DOE
+print(s_name.lower())  # john doe
+print("john" in s_name.lower())  # True
