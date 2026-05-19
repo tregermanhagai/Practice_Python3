79ee669bf682e94f5700f09db94b36d9ddb588bb
hagai.tregerman <hagai1973@gmail.com>
2026-05-19 18:27:08 +0300

    Add basic var file

---
+++ Variables/basic_var.py
@@ -0,0 +1,19 @@
+a = 2
+b = 50
+
+print(a + b)
+
+c = "Hello, World! "
+d = "How are you?"
+
+print(c + d)
+
+boo_var = b > 2
+print(boo_var)
+b = 1
+boo_var = b > 2
+print(boo_var)
+
+name = "Alice"
+age = 30
+
